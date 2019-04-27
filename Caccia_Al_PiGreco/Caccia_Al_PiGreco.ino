0a82d9daec13e5c3d5026cd215459ccfc1c3f802
Marco Alessio <alessiomarco02@gmail.com>
2019-04-27 09:03:01 +0200

    Codice quasi ultimato
    Mancano i bonus ed i malus

--- Caccia_Al_PiGreco/Caccia_Al_PiGreco.ino
+++ Caccia_Al_PiGreco/Caccia_Al_PiGreco.ino
@@ -6,19 +6,25 @@ int button2;
 int button3;
 int button4;
 int button5;
-int lives = 3;
+int lives;
+int time_limit;
 int matches;
+int counter;
+int support;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte Heart[8] = { B00000, B01010, B11111, B11111, B11111, B01110, B00100, B00000 };
+byte PiGreco[8] = { B00000, B11111, B01010, B01010, B01010, B01010, B00000, B00000 };
 
 void setup() {
   // put your setup code here, to run once:
   button_start = 2;
-  button1 = 2;
-  button2 = 3;
-  button3 = 4;
-  button4 = 5;
-  button5 = 6;
+  button1      = 4;
+  button2      = 6;
+  button3      = 8;
+  button4      = 10;
+  button5      = 12;
   pinMode(button_start, INPUT);
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
@@ -27,35 +33,108 @@ void setup() {
   pinMode(button5, INPUT);
   lcd.init();
   lcd.backlight();
+  lcd.createChar(0, Heart);
+  lcd.createChar(1, PiGreco);
+  start();
 }
 
-void generic_loop(int cell_start, int cell_end, int button){
-  int time_limit = 4000;
-  int cell = random(16);
+void generic_loop(int cell, int button){
   delay(2000);
   lcd.setCursor(cell, 1);
-  lcd.print("π");
-  int counter = 0;
-  if(cell_start < cell < cell_end){
-    while(digitalRead(button) == LOW){
+  lcd.write(byte(1));
+  counter = 0;
+  support = 1;
+  while(digitalRead(button) == LOW && counter < time_limit){
+    counter++;
+    delay(1);
+    if((button != button1 && digitalRead(button1) == HIGH) || (button != button2 && digitalRead(button2) == HIGH) || (button != button3 && digitalRead(button3) == HIGH) || (button != button4 && digitalRead(button4) == HIGH) || (button != button5 && digitalRead(button5) == HIGH)){
+      while(support < 2){
+        lives--;
+        support++;
+      }
       counter++;
-      delay(1);
+      lcd.setCursor(4, 0);
+      lcd.print(lives);
     }
-    lcd.clear();
-    if(counter > time_limit)
-      lives--;
-    else
-      time_limit -= 200;
   }
+  
+  clean();
+  if(counter == time_limit)
+    lives--;
+  else if(counter < 80){
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("NON IMBROGLIARE!");
+    lcd.setCursor(4, 1);
+    lcd.print("- 1 VITA");
+    delay(3000);
+    lives--;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.write(byte(0));
+    lcd.print(" = ");
+    lcd.setCursor(8, 0);
+    lcd.print("WON = ");
+  }
+  else{
+    if(time_limit > 300)
+      time_limit -= 150;
+    else
+      time_limit -= 50;
+    matches++;
+  }
+  lcd.setCursor(4, 0);
+  lcd.print(lives);
+  lcd.setCursor(14, 0);
+  lcd.print(matches);
+}
+
+void clean(){
+  for(int i = 0; i < 16; i++){
+    lcd.setCursor(i, 1);
+    lcd.print(" ");
+  }
+}
+
+void start(){
+  lcd.clear();
+  time_limit = 4050;
+  lives      = 3;
+  matches    = 0;
+  while(digitalRead(button_start) == LOW){
+    lcd.setCursor(0, 0);
+    lcd.print("INIZIA A GIOCARE");
+  }
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.write(byte(0));
+  lcd.print(" = "+ (String)lives);
+  lcd.setCursor(8, 0);
+  lcd.print("WON = " + (String)matches);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(button_start) == HIGH){
-    generic_loop(0, 3, button1);
-    generic_loop(4, 6, button2);
-    generic_loop(7, 9, button3);
-    generic_loop(10, 13, button4);
-    generic_loop(14, 16, button5);
+  if(lives > 0 && time_limit > 0){
+    int n_random = random(1, 5);
+    if(n_random == 1)
+      generic_loop(0, button1);
+    else if(n_random == 2)
+      generic_loop(4, button2);
+    else if(n_random == 3)
+      generic_loop(8, button3);
+    else if(n_random == 4)
+      generic_loop(12, button4);
+    else if(n_random == 5)
+      generic_loop(16, button5);
   }
+  else{
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("PUNTI TOTALI:");
+    lcd.setCursor(7, 1);
+    lcd.print(matches);
+    delay(3000);
+    start();
+  } 
 }
